ea711a12f6e8fc9369875d1fd920750b21572926
Mutiu Alabi-Bello <mutiualabi-bello@Mutius-MacBook-Pro.local>
2026-06-27 12:16:28 +0100

    Task 2

---
+++ Module4/src/Task2.java
@@ -0,0 +1,18 @@
+public class Task2 {
+    public static void main(String[] args) {
+
+        String name = "Maryam";
+        int age = 34;
+        double height = 5.5;
+        char grade = 'A';
+        boolean isLearningJava = true;
+        long phoneNumber = 447643793902L;
+
+        System.out.println(name);
+        System.out.println(age);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isLearningJava);
+        System.out.println(phoneNumber);
+    }
+}
