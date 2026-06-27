4e3c2e53ae805c0b36cf4f56923bc305894e1ff1
Mutiu Alabi-Bello <mutiualabi-bello@Mutius-MacBook-Pro.local>
2026-06-27 11:44:39 +0100

    Module 4 task 1

---
+++ Module4/src/Task1.java
@@ -0,0 +1,8 @@
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("My name is Maryam");
+        System.out.println("This is my first attempt learning Java);
+        System.out.println("This is Task 1 for Module 4.");
+    }
+}
